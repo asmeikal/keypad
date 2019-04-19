2143f17aed719a33273d0528a1e65b378bf123f0
Michelle Laurenti <asmeikal@me.com>
2019-04-19 09:24:30 +0200

    Add prettier and TSLint

--- src/__tests__/keypad.test.ts
+++ src/__tests__/keypad.test.ts
@@ -1,55 +1,55 @@
-import Decimal from "decimal.js";
+import Decimal from 'decimal.js';
 
 import { Keypad } from '../index';
 
 describe('Test keypad function', () => {
   it('checks default value', () => {
-    let d = new Keypad(7, 2);
-    expect(d.toString()).toBe("0.00");
+    const d = new Keypad(7, 2);
+    expect(d.toString()).toBe('0.00');
   });
 
   it('checks backspace behaviour', () => {
     let d = new Keypad(7, 2);
     d = d.backspace();
-    expect(d.toString()).toBe("0.00");
+    expect(d.toString()).toBe('0.00');
     d = d.addDigit(1);
-    expect(d.toString()).toBe("0.01");
+    expect(d.toString()).toBe('0.01');
     d = d.backspace();
-    expect(d.toString()).toBe("0.00");
+    expect(d.toString()).toBe('0.00');
   });
 
   it('checks max digits', () => {
     let d = new Keypad(7, 2);
     d = d.addDigit(1);
-    expect(d.toString()).toBe("0.01");
+    expect(d.toString()).toBe('0.01');
     d = d.addDigit(2);
-    expect(d.toString()).toBe("0.12");
+    expect(d.toString()).toBe('0.12');
     d = d.addDigit(3);
-    expect(d.toString()).toBe("1.23");
+    expect(d.toString()).toBe('1.23');
     d = d.addDigit(4);
-    expect(d.toString()).toBe("12.34");
+    expect(d.toString()).toBe('12.34');
     d = d.addDigit(5);
-    expect(d.toString()).toBe("123.45");
+    expect(d.toString()).toBe('123.45');
     d = d.addDigit(6);
-    expect(d.toString()).toBe("1234.56");
+    expect(d.toString()).toBe('1234.56');
     d = d.addDigit(7);
-    expect(d.toString()).toBe("12345.67");
+    expect(d.toString()).toBe('12345.67');
     d = d.addDigit(8);
-    expect(d.toString()).toBe("123456.78");
+    expect(d.toString()).toBe('123456.78');
     d = d.addDigit(9);
-    expect(d.toString()).toBe("1234567.89");
+    expect(d.toString()).toBe('1234567.89');
     d = d.addDigit(0);
-    expect(d.toString()).toBe("1234567.89");
+    expect(d.toString()).toBe('1234567.89');
     d = d.backspace();
-    expect(d.toString()).toBe("123456.78");
+    expect(d.toString()).toBe('123456.78');
   });
 
   it('checks double zero', () => {
     let d = new Keypad(7, 2);
     d = d.addDigit(1);
-    expect(d.toString()).toBe("0.01");
+    expect(d.toString()).toBe('0.01');
     d = d.addDoubleZero();
-    expect(d.toString()).toBe("1.00");
+    expect(d.toString()).toBe('1.00');
   });
 
   it('checks add double zero edge case', () => {
@@ -63,7 +63,7 @@ describe('Test keypad function', () => {
   });
 
   it('adds a non digit character', () => {
-    let d = new Keypad(7, 2);
+    const d = new Keypad(7, 2);
     expect(() => d.addDigit('c')).toThrow();
   });
 
@@ -73,35 +73,38 @@ describe('Test keypad function', () => {
     expect(d.toString()).toBe('0.01');
   });
 
-  it ('adds a negative number', () => {
-    let d = new Keypad(7, 2);
+  it('adds a negative number', () => {
+    const d = new Keypad(7, 2);
     expect(() => d.addDigit(-1)).toThrow();
   });
 
   it('adds two digits characters', () => {
-    let d = new Keypad(7, 2);
+    const d = new Keypad(7, 2);
     expect(() => d.addDigit('10')).toThrow();
   });
 
   it('adds a number greater than 10', () => {
-    let d = new Keypad(7, 2);
+    const d = new Keypad(7, 2);
     expect(() => d.addDigit(12)).toThrow();
   });
 
   it('tests concatenation', () => {
     let d = new Keypad(7, 2);
-    d = d.addDigit(1).addDigit(2).addDigit(3);
-    expect(d.toString()).toBe("1.23");
+    d = d
+      .addDigit(1)
+      .addDigit(2)
+      .addDigit(3);
+    expect(d.toString()).toBe('1.23');
   });
 
   it('tests Decimal value', () => {
     let d = new Keypad(7, 2);
     d = d.addDigit(1);
-    expect(new Decimal("0.01").equals(d.value)).toBeTruthy();
+    expect(new Decimal('0.01').equals(d.value)).toBeTruthy();
     d = d.addDigit(2);
-    expect(new Decimal("0.12").equals(d.value)).toBeTruthy();
+    expect(new Decimal('0.12').equals(d.value)).toBeTruthy();
     d = d.addDigit(3);
-    expect(new Decimal("1.23").equals(d.value)).toBeTruthy();
+    expect(new Decimal('1.23').equals(d.value)).toBeTruthy();
   });
 
   it('adds some zero at the beginning', () => {
@@ -110,18 +113,18 @@ describe('Test keypad function', () => {
     for (let i = 0; i < 8; ++i) {
       d = d.addDigit(0);
     }
-    expect(d.toString()).toBe("0.00");
+    expect(d.toString()).toBe('0.00');
 
     for (let i = 1; i <= 9; ++i) {
       d = d.addDigit(i);
     }
-    expect(d.toString()).toBe("1234567.89");
+    expect(d.toString()).toBe('1234567.89');
   });
 
   it('tests a different number of decimals', () => {
     let d = new Keypad(8, 8);
 
     d = d.addDigit(1);
-    expect(d.toString()).toBe("0.00000001");
+    expect(d.toString()).toBe('0.00000001');
   });
 });
--- src/keypad.ts
+++ src/keypad.ts
@@ -1,46 +1,61 @@
-import Decimal from "decimal.js";
+import Decimal from 'decimal.js';
 
 export class Keypad {
+  public readonly value: Decimal;
   private readonly digits: number;
   private readonly maxlog: Decimal;
-  private readonly decimal_places: number;
+  private readonly decimalPlaces: number;
 
-  readonly value: Decimal;
-
-  constructor(digits: number, decimals: number, value: Decimal | number | string = 0) {
+  constructor(
+    digits: number,
+    decimals: number,
+    value: Decimal | number | string = 0
+  ) {
     this.digits = digits;
     this.maxlog = new Decimal(this.digits).minus(1);
-    this.decimal_places = decimals;
+    this.decimalPlaces = decimals;
     this.value = this.cleanup(new Decimal(value));
   }
 
-  private cleanup(value: Decimal): Decimal {
-    return value.toDecimalPlaces(this.decimal_places, Decimal.ROUND_DOWN);
+  public backspace() {
+    return new Keypad(
+      this.digits,
+      this.decimalPlaces,
+      this.value.dividedBy(10)
+    );
   }
 
-  backspace() {
-    return new Keypad(this.digits, this.decimal_places, this.value.dividedBy(10));
-  }
-
-  addDigit(digit: string | number) {
+  public addDigit(digit: string | number) {
     if (typeof digit === 'string') {
       if (!/^\d$/.test(digit)) {
-        throw new Error("Digit should be a number between 0 and 9");
+        throw new Error('Digit should be a number between 0 and 9');
       }
     } else {
       if (digit < 0 || digit > 10) {
-        throw new Error("Digit should be between 0 and 9");
+        throw new Error('Digit should be between 0 and 9');
       }
     }
-    if (this.value.logarithm() > this.maxlog) return this;
-    return new Keypad(this.digits, this.decimal_places, this.value.times(10).plus(new Decimal(digit).dividedBy(Decimal.pow(10, this.decimal_places))));
+    if (this.value.logarithm() > this.maxlog) {
+      return this;
+    }
+    return new Keypad(
+      this.digits,
+      this.decimalPlaces,
+      this.value
+        .times(10)
+        .plus(new Decimal(digit).dividedBy(Decimal.pow(10, this.decimalPlaces)))
+    );
   }
 
-  addDoubleZero() {
+  public addDoubleZero() {
     return this.addDigit(0).addDigit(0);
   }
 
-  toString() {
-    return this.value.toFixed(this.decimal_places);
+  public toString() {
+    return this.value.toFixed(this.decimalPlaces);
+  }
+
+  private cleanup(value: Decimal): Decimal {
+    return value.toDecimalPlaces(this.decimalPlaces, Decimal.ROUND_DOWN);
   }
 }
