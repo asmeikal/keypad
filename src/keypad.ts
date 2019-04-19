d55ba2051318f60a8b6dfec17b525c48d699e6da
Michelle Laurenti <asmeikal@me.com>
2019-04-19 09:17:56 +0200

    Make Keypad immutable
    Update tests. Fix bug.

--- src/keypad.ts
+++ src/keypad.ts
@@ -1,31 +1,28 @@
 import Decimal from "decimal.js";
 
 export class Keypad {
-  private digits: Decimal;
-  private maxlog: Decimal;
-  private decimals: number;
+  private readonly digits: number;
+  private readonly maxlog: Decimal;
+  private readonly decimal_places: number;
 
-  value: Decimal;
+  readonly value: Decimal;
 
-  constructor(digits: number, decimals: number, value: number | string = 0) {
-    this.digits = new Decimal(digits);
-    this.maxlog = this.digits.minus(1);
-    this.decimals = decimals;
-    this.value = new Decimal(value);
+  constructor(digits: number, decimals: number, value: Decimal | number | string = 0) {
+    this.digits = digits;
+    this.maxlog = new Decimal(this.digits).minus(1);
+    this.decimal_places = decimals;
+    this.value = this.cleanup(new Decimal(value));
   }
 
-  private cleanup() {
-    this.value = this.value.toDecimalPlaces(this.decimals, Decimal.ROUND_DOWN);
+  private cleanup(value: Decimal): Decimal {
+    return value.toDecimalPlaces(this.decimal_places, Decimal.ROUND_DOWN);
   }
 
   backspace() {
-    this.value = this.value.dividedBy(10);
-    this.cleanup();
-    return this;
+    return new Keypad(this.digits, this.decimal_places, this.value.dividedBy(10));
   }
 
   addDigit(digit: string | number) {
-    if (this.value.logarithm() > this.maxlog) return;
     if (typeof digit === 'string') {
       if (!/^\d$/.test(digit)) {
         throw new Error("Digit should be a number between 0 and 9");
@@ -35,18 +32,15 @@ export class Keypad {
         throw new Error("Digit should be between 0 and 9");
       }
     }
-    this.value = this.value.times(10).plus(new Decimal(digit).dividedBy(100));
-    this.cleanup();
-    return this;
+    if (this.value.logarithm() > this.maxlog) return this;
+    return new Keypad(this.digits, this.decimal_places, this.value.times(10).plus(new Decimal(digit).dividedBy(Decimal.pow(10, this.decimal_places))));
   }
 
   addDoubleZero() {
-    this.addDigit(0);
-    this.addDigit(0);
-    return this;
+    return this.addDigit(0).addDigit(0);
   }
 
   toString() {
-    return this.value.toFixed(this.decimals);
+    return this.value.toFixed(this.decimal_places);
   }
 }
